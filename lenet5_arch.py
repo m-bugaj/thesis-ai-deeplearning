b86cadf17996d77e3eb7d58b2c151beef3d3a0f1
m-bugaj <bbugi2016@gmail.com>
2023-07-07 12:00:00 +0200

    Updated soft for new optimizers

--- lenet5_arch.py
+++ lenet5_arch.py
@@ -53,7 +53,7 @@ class LeNet5:
 
         df = pd.DataFrame(history)
         
-        fig, ax1 = plt.subplots(figsize=(8, 5))
+        fig, ax1 = plt.subplots(figsize=(9, 6))
 
         # Skala po lewej stronie dla accuracy
         ax1.plot(df.index, df['accuracy'], 'b-', label='Accuracy')
@@ -74,7 +74,7 @@ class LeNet5:
             else:
                 model_params[key] = value
         
-        title_part = f"Batch size: {model_params['bs']}, Activation function: {model_params['af']}, Learning rate: {model_params['lr']}"
+        title_part = f"Batch size: {model_params['bs']}, Activation function: {model_params['af']},\n Optimizer: {model_params['opt']}"
 
         ax1.set_title('Training History + GPU Usage:\n {} - {}'.format(arch_name, title_part))
 
@@ -110,7 +110,7 @@ class LeNet5:
         history_df = pd.DataFrame(history)
         gpu_usage_df = pd.DataFrame(gpu_usage_data)
         
-        fig, ax1 = plt.subplots(figsize=(8, 5))
+        fig, ax1 = plt.subplots(figsize=(9, 6))
 
         ax1.set_xlabel('Epoch')
         ax1.set_ylabel('Accuracy / Loss')
@@ -131,7 +131,7 @@ class LeNet5:
             else:
                 model_params[key] = value
         
-        title_part = f"Batch size: {model_params['bs']}, Activation function: {model_params['af']}, Learning rate: {model_params['lr']}"
+        title_part = f"Batch size: {model_params['bs']}, Activation function: {model_params['af']},\n Optimizer: {model_params['opt']}"
 
         ax1.set_title('Training History + GPU Usage:\n {} - {}'.format(arch_name, title_part))
         
@@ -238,7 +238,7 @@ class LeNet5:
 
 
     
-    def train_model(self, model_name, arch_name, compile_optimizer, compile_loss, fit_epochs, fit_batch_size, activation_function, log_custom_dir=''):
+    def train_model(self, model_name, arch_name, compile_optimizer, compile_loss, fit_epochs, fit_batch_size, activation_function, log_custom_dir='', callbacks=[]):
 
         # Ustawienie seed
         seed = 10937
@@ -296,7 +296,7 @@ class LeNet5:
             class_mode='categorical',
             color_mode='grayscale',
             seed=seed,
-            shuffle=False  # ON / OFF shuffling of data
+            shuffle=True  # ON / OFF shuffling of data
         )
 
         test_generator = test_datagen.flow_from_directory(
@@ -306,7 +306,7 @@ class LeNet5:
             class_mode='categorical',
             color_mode='grayscale',
             seed=seed,
-            shuffle=False  # ON / OFF shuffling of data
+            shuffle=True  # ON / OFF shuffling of data
         )
 
         # Konfiguracja TensorBoard
@@ -319,6 +319,7 @@ class LeNet5:
         log_gpu_usage_callback = GPUUsageLogger()
         profiler_callback = ProfilerCallback(log_dir=log_dir, log_frequency=8)
         measuring_time = MeasuringTime()
+        lr_logger_callback = LearningRateLogger()
 
         model_file_path = os.path.join('models', arch_name)
         if not os.path.exists(model_file_path):
@@ -368,7 +369,8 @@ class LeNet5:
                                              tensorboard_callback, 
                                              image_callback, 
                                              profiler_callback,
-                                             model_checkpoint_callback])
+                                             model_checkpoint_callback,
+                                             lr_logger_callback] + callbacks)
         
         stop_time = time.time()
 
@@ -627,3 +629,10 @@ class MeasuringTime(tf.keras.callbacks.Callback):
     def on_train_end(self, logs=None):
         print("Training time: {}".format(self.total_time))
         return self.total_time
+    
+class LearningRateLogger(tf.keras.callbacks.Callback):
+    def on_epoch_begin(self, epoch, logs=None):
+        lr = self.model.optimizer.lr
+        if hasattr(lr, 'numpy'):
+            lr = lr.numpy()
+        print(f'Epoch {epoch + 1}: Current learning rate is {lr}')
